60ebfb66748bed5e152efb66bd957fb348af56cb
jane.zhou <jane.zhou@digitwin.com>
2022-10-25 18:17:18 +0800

    no message

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,52 @@
+# Checked Date: 2022/10/25
+
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+# Example 1:
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+
+# Example 2:
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+#
+#
+# Constraints:
+#
+# 1 <= n <= 45
+
+from typing import List
+
+
+def climbStairs(n: int) -> int:
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        dp = [0 for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        i = 2
+        while i < n:
+            dp[i] = dp[i - 1] + dp[i - 2]
+            i += 1
+        a = int(dp[i - 1])
+        return a
+
+
+if __name__ == "__main__":
+    print(climbStairs(n = 38))
